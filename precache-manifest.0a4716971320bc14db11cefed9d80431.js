e8b08484dcd113a54695c88359ded310bb83efaf
Kushal Rajkarnikar <kushal.rajkarnikar@gmail.com>
2018-11-25 17:25:56 +0545

    Updates

---
+++ precache-manifest.0a4716971320bc14db11cefed9d80431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "ab795e6f974258bb776e",
+    "url": "/memory-game/static/js/main.ab795e6f.chunk.js"
+  },
+  {
+    "revision": "36620ee202d88a294064",
+    "url": "/memory-game/static/js/1.36620ee2.chunk.js"
+  },
+  {
+    "revision": "ab795e6f974258bb776e",
+    "url": "/memory-game/static/css/main.a389c751.chunk.css"
+  },
+  {
+    "revision": "f55a5705ec23517edaf7f6b951f8f2a0",
+    "url": "/memory-game/index.html"
+  }
+];
